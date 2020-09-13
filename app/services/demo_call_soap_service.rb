1499527b77c53f2033094047fb67ac2fbb0dcdb7
Phuc Nguyen <nguyenhuyphuc0412@gmail.com>
2020-09-13 09:56:05 +0700

    Update demo_call_soap_service.rb

--- app/services/demo_call_soap_service.rb
+++ app/services/demo_call_soap_service.rb
@@ -15,7 +15,7 @@ class DemoCallSoapService
     request.body = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\r\n  <soap:Body>\r\n    <NumberToWords xmlns=\"http://www.dataaccess.com/webservicesserver/\">\r\n      <ubiNum>501</ubiNum>\r\n    </NumberToWords>\r\n  </soap:Body>\r\n</soap:Envelope>\r\n"
 
     response = https.request(request)
-    parse_response(response)
+    parse_response(response.body)
   end
 
   def parse_response(response)
